fee7c5053be6c5cd28b8df82fc4ef0585b9665f5
Drew Thompson <drewallynthompson@gmail.com>
2020-12-06 08:32:58 +0000

    Automatically backed up by Learn

--- db/migrate/20201206082856_create_preferences.rb
+++ db/migrate/20201206082856_create_preferences.rb
@@ -1,7 +1,7 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
-      t.boolean :allow_create_artists
+      t.boolean :allow_create_artists, default
       t.boolean :allow_create_songs
 
       t.timestamps
